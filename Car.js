ab626aae2617c84e7ee4b8edd23667736d3bc0e1
Lazuardi Fatahilah Hamdi <lazuardifath@gmail.com>
2021-04-11 13:46:20 +0700

    change again -_-

--- Car.js
+++ Car.js
@@ -1,236 +1,41 @@
-window.Car = function(genes){
-    this.pos = new Point(car_h/2 + 200, height/2);
-    this.acc = new Point(0, 0);
-    this.vel = new Point(0, 0);
-    
-    this.maximum_wheel_angle = 30;
-    this.current_wheel_angle = 0;
-    this.car_angle = 0;
-    this.prev_car_angle = 0;
-    this.delta_angle = 0;
-    this.tr = 0;
-    
-    this.color = new Color(Math.random() * 255, Math.random() * 255, Math.random() * 255);
-    
-    this.fitness = 1; 
-    this.is_dead = false;
-    
-    this.genes = genes || [];
-    this.next_genes = [];
-    this.update_counter = 0
-    
-    this.tire_w = 20;
-    this.tire_h = 10;
-    this.rac = new Point(axis_h/2, 0);; // rare axle center
-    
-    this.circle_pos = 0;
-    
-    // draw car body and wheels
-    
-    var pos_shape = new Path.Circle(this.pos, 5);
-    pos_shape.fillColor = 'white';
-
-    var car_body = new Path.Rectangle(- car_h/2, - car_w/2, car_h, car_w);
-    car_body.fillColor = rgba(0, 0, 0, 0.1);
-    car_body.strokeColor = 'green';
-    car_body.position = this.pos;
-
-
-    // wheels
-    var front_up = new Path.Rectangle(0, 0, this.tire_w, this.tire_h);
-    front_up.fillColor = rgba(255, 0, 0, 0.5);
-    front_up.strokeColor = 'green';
-    front_up.rotate(this.current_wheel_angle);
-    front_up.position.x = this.pos.x - axis_h/2;
-    front_up.position.y = this.pos.y - car_w/2;
-
-
-    var rare_up = new Path.Rectangle(0, 0, this.tire_w, this.tire_h);
-    rare_up.fillColor = rgba(0, 0, 0, 0.1);
-    rare_up.strokeColor = 'green';
-    rare_up.position.x = this.pos.x + axis_h/2;
-    rare_up.position.y = this.pos.y - car_w/2;
-    
-    rare_a = this.pos.clone();
-    rare_a.x = axis_h/2;
-    rare_a.y = -car_w / 2;
-
-    rare_b = this.pos.clone();
-    rare_b.x = axis_h/2;
-    rare_b.y = car_w / 2;
-    
-    front_a = this.pos.clone();
-    front_a.x = -axis_h/2;
-    front_a.y = -car_w / 2;
-    
-    front_b = this.pos.clone();
-    front_b.x = -axis_h/2;
-    front_b.y = car_w / 2;
-    
-    tr_shape = new Path.Circle(new Point(0, 0), 1);
-    tr_shape.strokeColor = 'green';
-    
-    radius_axle_line = new Path.Line(0, 0);
-    radius_axle_line.strokeColor = 'green';
-    radius_axle_line.dashArray = [5, 5];
-    
-    rare_axle_line = new Path.Line(0, 0);
-    rare_axle_line.strokeColor = rgb(255, 0, 255);
-    
-    radius_line = new Path.Line(0, 0);
-    radius_line.strokeColor = 'green';
-    radius_line.dashArray = [10, 10];
-    
-    new_rac_shape = new Path.Circle(new Point(0, 0), 5);
-    new_rac_shape.strokeColor = rgb(0, 0, 255);
-    new_rac_shape.fillColor = rgb(0, 0, 255);
-    
-    new_pos_shape = new Path.Circle(new Point(0, 0), 5);
-    new_pos_shape.strokeColor = rgb(0, 255, 0);
-    new_pos_shape.fillColor = rgb(0, 255, 0);
-    
-    rac_shape = new Path.Circle(new Point(0, 0), 5);
-    rac_shape.strokeColor = 'red';
-    rac_shape.fillColor = 'red';
-    
-    this.update = function(){
-        if (this.is_dead) return;
-        
-        // reset wheel angles
-        front_up.rotate(-this.current_wheel_angle);
-        this.prev_car_angle = this.car_angle;
-        car_body.position = this.pos;
-        pos_shape.position = this.pos;
-
-        // get new iteration parameter
-        var impulse;
-        if (this.genes.length > 0 && this.genes.length > this.update_counter){
-            impulse = this.genes[this.update_counter];
+class Car{
+    constructor(){
+        this.pos=createVector(width/2,height-50);
+        this.velocity=createVector();
+        this.acceleration=createVector();
+        this.genes=[];
+        this.fitness=0;
+        this.dead=false;
+        this.win=false;
+        for(var i=0;i<lifespan;i++){
+            this.genes[i]=p5.Vector.random2D();
+            this.genes[i].setMag(0.15);
         }
-        else {
-            impulse = Math.random() * 10 - 5; // random rotation
-        }
-        this.next_genes.push(impulse); // save for further generations
-        
-        this.current_wheel_angle = Math.min(this.maximum_wheel_angle, Math.max(-this.maximum_wheel_angle, this.current_wheel_angle + impulse));
-//        this.current_wheel_angle = 20;
-
-        
-        // calculate turning radius and travelled arc length
-        this.tr = axis_h / sin(radians(this.current_wheel_angle));
-        turning_circle_len = abs(PI * 2 * this.tr);
-        arc_angle = 20 / this.tr; // 20 is default
-
-        this.circle_pos = this.pos.clone();
-        this.circle_pos.length = this.tr;
-
-        // calculate part centres
-        rare_a.x = this.pos.x + axis_h/2;
-        rare_a.y = this.pos.y - car_w/2;
-        rare_b.x = this.pos.x + axis_h/2;
-        rare_b.y = this.pos.y + car_w/2;
-        rare_a.rotate(this.car_angle, this.rac);
-        rare_b.rotate(this.car_angle, this.rac);
-        
-        front_a.x = this.pos.x - axis_h/2;
-        front_a.y = this.pos.y - car_w/2;
-        front_b.x = this.pos.x - axis_h/2;
-        front_b.y = this.pos.y + car_w/2;
-        
-        axle_a_b = rare_b.clone().subtract(rare_a)
-        axle_a_b.length = - this.tr;
-        this.tr += car_w / 2;
-        radius_center = rare_a.clone().add(axle_a_b);
-        
-        //tr_shape = new Path.Circle(radius_center, this.tr);
-        tr_shape.position = radius_center;
-        var tmp_r = tr_shape.bounds.width / 2;
-        tr_shape.scale(this.tr / tmp_r);
-        
-
-        radius_axle_line.segments[0].point = rare_a;
-        radius_axle_line.segments[1].point = radius_center;
-
-        rare_axle_line.segments[0].point = rare_a;
-        rare_axle_line.segments[1].point = rare_b;
-        
-        
-        // center of rare axle
-        this.rac = new Point(axis_h/2, 0);
-        this.rac.rotate(this.car_angle)
-        this.rac.x = this.pos.x + axis_h/2;
-        this.rac.y += this.pos.y;
-        rac_shape.position = this.rac;
-
-        radius_line.segments[0].point = radius_center;
-        radius_line.segments[1].point = this.pos;
-        
-        cntr_to_pos = this.pos.clone().subtract(radius_center);
-        cntr_to_rac = this.rac.clone().subtract(radius_center);
-
-        if (this.current_wheel_angle > 0){
-            new_arc_angle = cntr_to_rac.angleInRadians - arc_angle;
-            new_rac = new Point(radius_center.x + this.tr * cos(new_arc_angle), radius_center.y + this.tr * sin(new_arc_angle));
-        }
-        else {
-            new_arc_angle = cntr_to_rac.angleInRadians - arc_angle + PI;
-            new_rac = new Point(radius_center.x + this.tr * cos(new_arc_angle), radius_center.y + this.tr * sin(new_arc_angle));
-        }
-        
-        new_rac_shape.position = new_rac;
-        
-        new_pos = this.pos.clone().subtract(this.rac.clone().subtract(new_rac))
-        new_pos_shape.position = new_pos;
-
-        this.car_angle += new_arc_angle;
-        this.delta_angle = this.car_angle - this.prev_car_angle;
-        this.delta_angle = -this.delta_angle;
-        console.log('car_angle at end', this.car_angle)
-        
-        car_body.rotate(this.delta_angle);
-        car_body.rotate(this.delta_angle, radius_center);
-        
-        front_up.rotate(this.current_wheel_angle);
-        front_up.rotate(this.delta_angle, radius_center)
-        rare_up.position = rare_a;
-        rare_up.rotate(this.delta_angle, radius_center)
-        front_up.position = front_a;
-        
-        newrac_to_rac = this.rac.clone().subtract(new_rac);
-        newrac_to_rac.rotate(this.car_angle);
-        this.pos = new_pos;
-        
-        this.update_counter++;
     }
-    
-    this.evaluate = function(){
-        
+    applyForce(force){
+        this.acceleration.add(force);
     }
-    
-    this.constrain_to_screen = function(){
-        // don't get outside the screen
-        if (this.pos.y > height){ // on floor
-            this.is_dead = true;
+    update(){
+        if(!this.dead && !this.win){
+            this.velocity.add(this.acceleration);
+            this.pos.add(this.velocity);
+            this.acceleration.mult(0);
         }
-        if (this.pos.x > width){ // on left
-            this.is_dead = true;
+        if(collideRectRect(this.pos.x,this.pos.y,30,6,target.x,target.y,10,10)){
+            this.win=true;   
         }
-        if (this.pos.x < 0){ // on right
-            this.is_dead = true;
+        if(collideRectRect(this.pos.x,this.pos.y,30,6,ob.x,ob.y,300,20)|| this.pos.x>width ||this.pos.x<0 ||this.pos.y>height ||this.pos.y<0){
+            this.dead=true;
         }
-        
-        if (this.is_dead){ // remove the defective gene
-            this.next_genes.splice(this.jump_counter-1, 1);
-        }
-        // do we care about shooting above top? probably not
     }
-    
-    this.show = function(){
-        
-        
-    }
-    
-    this.hits = function(obstacle){
-        
+    show(){
+        stroke(255);
+        fill(255,100);
+        rectMode(CENTER);
+        push();
+        translate(this.pos.x,this.pos.y)
+        rotate(this.velocity.heading());
+        rect(0,0,30,6);
+        pop();
     }
 }
--- Population.js
+++ Population.js
@@ -1,157 +1,85 @@
-function Population(cnt){
-    this.cnt = cnt;
-    this.initial_lifespan = 300;
-    this.lifespan = this.initial_lifespan;
-    
-    this.members = [];
-    this.new_members = [];
-    this.mating_pool = [];
-    this.last_mating_pool = [];
-    
-    for (var i = 0; i < this.cnt; i++){
-        this.members.push(new Car());
-    }
-
-    this.setPopulationSize = function(n){
-        this.cnt = n;
-    }
-    
-    this.update = function(){
-        cnt_dead = 0;
-        for (var i = 0; i < this.members.length; i++){
-//            this.members[i].applyForce([random(0, 0.1), 0]);
-            this.members[i].update();
-            this.members[i].evaluate();
-            if (this.members[i].is_dead) cnt_dead++;
-        }
-
-        // don't wait til the end of lifespan, if all members are dead
-        if (this.members.length == cnt_dead){
-            this.lifespan = 0;
+class Population{
+    constructor(){
+        this.cars=[];
+        this.populationCount=50;
+        this.count=0;
+        this.generation=0;
+        this.pool=[];
+        for(var i=0;i<this.populationCount;i++){
+            this.cars.push(new Car());
         }
     }
-    
-    this.show = function(){
-        for (var i = 0; i < this.members.length; i++){
-            this.members[i].show();
+    update(){
+        for(var i in this.cars){
+            this.cars[i].applyForce(this.cars[i].genes[this.count]);
+            this.cars[i].update();
+        }
+        this.count++;
+        if(this.count==lifespan){
+            this.count=0;
+            lifespan+=10;
+            this.generation++;
+            document.getElementById("gen").innerHTML="Generation= "+this.generation;
+            this.fitness();
+            document.getElementById("fit").innerHTML="Max fitness= "+max;
+            this.selection();
         }
     }
-    
-    this.evaluate = function(){
-        var $fitnesses = $('#fitnesses');
-        $fitnesses.empty();
-        $fitnesses.append('<tr><th>Car</th><th>Fitness</th><th>Mating slots</th><th>Angle penalty</th></tr>');
-
-        var sum_fitness = 0;
-        for (var i = 0; i < this.members.length; i++){
-            // this.members[i].evaluate();
-            sum_fitness += this.members[i].max_fitness;
-        }
-        $('.sum_fitness').html(sum_fitness);
-        $('.average_fitness').html(sum_fitness / this.members.length);
-
-        if (sum_fitness > current_record) current_record = sum_fitness;
-
-        // stretch population fitness to 0-100 range
-        var min_fitness = 100;
-        for (var i = 0; i < this.members.length; i++){
-            if (this.members[i].max_fitness < min_fitness) min_fitness = this.members[i].max_fitness;
-        }
-        // console.log('min fitness is', min_fitness)
-        for (var i = 0; i < this.members.length; i++){
-            this.members[i].allowed_mating_slots = map(this.members[i].max_fitness, min_fitness, 100, 0, 100);
-            this.members[i].allowed_mating_slots = Math.ceil(this.members[i].allowed_mating_slots / 10);
-            this.members[i].allowed_mating_slots += 1; // make sure everyone has at least one slot
-                // console.log(i, 'allowed slots', this.members[i].allowed_mating_slots)
-        }
-        
-        this.mating_pool = [];
-        for (var i = 0; i < this.members.length; i++){
-            $fitnesses.append('<tr><td>#'+i+': </td><td>'+this.members[i].max_fitness+'</td><td>'+
-                this.members[i].allowed_mating_slots+'</td><td>-'+this.members[i].heading_penalty+'</td></tr>');
-
-            for (var j = 0; j < this.members[i].allowed_mating_slots; j++){
-                this.mating_pool.push(this.members[i].next_genes);
+    fitness(){
+        max=0;
+        for(var i in this.cars){
+            var d=dist(this.cars[i].pos.x,this.cars[i].pos.y,target.x,target.y);
+            this.cars[i].fitness=map(d,0,width,width,0);
+            if(this.cars[i].win){
+                this.cars[i].fitness*=10;
             }
+            else if(this.cars[i].dead){
+                this.cars[i].fitness/=20;
+            }
+            if(this.cars[i].fitness>max){
+                max=this.cars[i].fitness;
+            }   
         }
-        $fitnesses.append('<tr><td>M. pool</td><td>'+this.mating_pool.length+'</td></tr>');
-        if (this.mating_pool.length == 0){
-            // console.log('empty pool', this.members)
-            run = false;
+        for(var i in this.cars){
+            this.cars[i].fitness/=max;
         }
-
     }
-    
-    this.crossover = function(){
-        this.new_members = [];
-        // console.log('begin to cross, and pool is', this.mating_pool.length)
-        // for (var i = 0; i < this.cnt; i++){
-        for (var i = 0; this.new_members.length < this.cnt; i++){
-            var next_genes = [];
-            var idxA = Math.floor(Math.random() * this.mating_pool.length)
-            var idxB = Math.floor(Math.random() * this.mating_pool.length)
-            var parentA = this.mating_pool[idxA];
-            var parentB = this.mating_pool[idxB];
-            if (typeof parentA == 'undefined' || typeof parentB == 'undefined') {
-                console.log('cant select parent:', idxA, idxB, this.mating_pool)
-                break;
+    selection(){
+        this.pool=[];
+        var newCars=[];
+        for(var i in this.cars){
+            for(var j=0;j<(this.cars[i].fitness*100);j++){
+                this.pool.push(this.cars[i]);
             }
-            
-            var longer_parent, shorter_parent;
-            if (parentA.length > parentB.length){
-                longer_parent = parentA;
-                shorter_parent = parentB;
-            }
-            else {
-                longer_parent = parentB;
-                shorter_parent = parentA;
-            }
-            
-            var split_mid = Math.round(Math.random() * shorter_parent.length);
-            for (var j = 0; j < split_mid; j++){
-                next_genes[j] = parentA[j];
-            }
-            for (var j = split_mid; j < longer_parent.length; j++){
-                if (typeof parentB[j] != 'undefined'){
-                    next_genes[j] = parentB[j];
+        }
+        for(var i=0;i<this.populationCount;i++){
+            var index=floor(random(this.pool.length));
+            var pa=this.pool[index];
+            index=floor(random(this.pool.length));
+            var pb=this.pool[index];
+            var child=new Car();
+            var mid=pa.genes.length/2;
+            for(var j=0;j<pa.genes.length;j++){
+                if(j<mid){
+                    child.genes[j]=pa.genes[j];
                 }
-                else {
-                    next_genes[j] = longer_parent[j];
+                else{
+                    child.genes[j]=pb.genes[j];
                 }
             }
-
-            // mutation
-            if (Math.random() * 100 < iMutationRate){
-                var gene_idx = Math.round(Math.random() * next_genes.length);
-                var gene_value = Math.random() * 20 - 10;
-                // console.log('Mutation! gene', gene_idx, 'from', next_genes[gene_idx], 'to', gene_value)
-                next_genes[gene_idx] = gene_value;
+            for(var j=0;j<pa.genes.length;j++){
+                if(random(1)<0.001){
+                    child.genes[j]=p5.Vector.random2D();
+                    child.genes[j].setMag(0.15);
+                }
             }
-
-            if (i == 0){
-                // lets give the 0th member a set of fresh genes
-                next_genes = [];
-            }
-
-            this.new_members.push(new Car(next_genes));
+            newCars.push(child);
         }
+        this.cars=newCars;
     }
-
-    this.purge = function(){
-        for (var i = 0; i < this.members.length; i++){
-            this.members[i].remove();
+    show(){
+        for(var i in this.cars){
+            this.cars[i].show();
         }
-
-        if (this.new_members.length == 0){
-            run = false;
-            console.log('NO NEW MEMBERS')
-        }
-
-        this.members = this.new_members;
-        // console.log('purge, new members:', this.members.length)
-    }
-    
-    this.resurrect = function(){
-        this.lifespan = this.initial_lifespan;
     }
 }
